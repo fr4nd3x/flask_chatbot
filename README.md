c0ee911c3f69ad8e87e32ac23c48ea124a9c2bed
fr4nd3x <francisco-dg-1@hotmail.com>
2022-11-10 09:58:20 -0500

    url edit

---
+++ README.md
@@ -0,0 +1,3 @@
+https://www.regionancash.gob.pe/listaconsejeros.php
+
+
--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+import webbrowser
 
 app = Flask(__name__)
 cors = CORS(app)
-
-
+def url():
+    webbrowser.open('http://sisgedo.regionancash.gob.pe/sisgedonew/app/main.php')
+    return 
 
 
 @app.route('/', methods = ['POST'])
@@ -18,53 +20,67 @@ def index():
     
     select = {
         '1':{'ID':1,'msg': 'Hola , en que te puedo ayudar?','options':[
-            {"label":"Documento","next":''},
+            {"label":"Documento","next":'8'},
             {"label":"Contacto","next":'2'}
         ]},
         
         '2':{'ID':2,'msg':'Bien, seleccione una opcion', 'options':[
-            {"label":"Ambiental","next":'3'},
-            {"label":"Urbanismo","next":'3'}
+            {"label":"Autoridad ","next":'3'},
+            {"label":"Sector","next":'4'},
+            {"label":"Instituciones","next":''}
         ]},
+            
             
         '3':{'ID':3, 'msg':'Selecione la persona con quien quiere contactarse','options':[
-            {"label":"Autoridad","next":'5'},
-            {"label":"Personal","next":'4'}
+            {"label":"Gobernador Regional","next":''},
+            {"label":"Consejeros Regionales ","next":'7'}
             
         ]},
         
+        
         '4':{'ID':4,'msg':'Selecione al personal','options':[
-            {"label":"Jorge"},
-            {"label":"Arellano"},
-            {"label":"Antonio"}
+            {"label":"Gerencia","next":"5"},
+            {"label":"Personal","next":"6"}
         ]},
         
+        
         '5':{'ID':5,'msg':'Selecione a la autoridad','options':[
-            {"label":"Gobernador","next":'6'},
-            {"label":"Consejero","next":'6'},
             {"label":"Gerente","next":'6'},
             {"label":"Sub-gerete","next":'6'}
         ]},
         
         '6':{'ID':6,'msg':'Seleccione a la persona', 'options':[
-            {"label":"Juan"},
-            {"label":"Ruben"},
-            {"label":"Adiran"}
+            {"label":"Gerencia General","next":''},
+            {"label":"Gerencia Regional ","next":''},
+            {"label":"Gerencia Regioonla de Asesoria Juridica ","next":''},
+            {"label":"Gerencia Regional de Planeamiento","next":''},
+            {"label":"Gerencia Regional de Desarrollo Social","next":''},
+            {"label":"Gerencia Regional de Desarrollo Economico","next":''},
+            {"label":"Gerencia Regional de Infraestructura","next":''},
+            {"label":"Gerencia Regional de Recursos Naturales y Gestion Ambiental","next":''}
         ]},
-        '7':{'ID':7,'input':'Escriba un codigo:'},
-        }
-    
         
+        '7':{'ID':7,"msg":"Seleccione a la persona",'options':[
+            {"label":"Oficina de imagen institucional","next":''},
+            {"label":"Secretaria General ","next":''},
+            {"label":"Oficina regional de defensa nacional","next":''}
+            
+            
+            ]},
+
+        "8":{"ID":8, 'msg':'<div>Ingrese al siguiente link :','href':'http://sisgedo.regionancash.gob.pe/sisgedonew/app/main.php',"option":[{}]},
+
+        "9":{"ID":9,'msg':'Ingrese al siguiente link: <a href="#">http://sisgedo.regionancash.gob.pe/sisgedonew/app/main.php</a>','options':[{}]},
+
+        "10":{"ID":10,'msg':'Deseas continuar?',"options":[
+            {"label":"SI","next":"1"},
+            {"label":"NO","next":"11"}
+        ]},
+        "11":{"ID":11, 'msg':"Adios"}
+        }    
     return jsonify(select[option])
 
 
-@app.route("/code", Method =['POST'])
-def code ():
-    o = request.json
-    input = o    
-    str(input("Ingrese un codigo:"))
-    return input
 
 
-
-#str(input("Ingrese el cofigo del documento:"))
+#<a href="/helo">aaaa</a></div>
